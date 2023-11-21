7af0220f7c27d5d1c80e01088170d8a5b809f147
Joelle  Johnson <joellej221@gmail.com>
2023-11-21 16:52:32 -0500

    fixed glitch on refresh

--- app/dashboard/CheckAuth.js
+++ app/dashboard/CheckAuth.js
@@ -4,19 +4,24 @@ import { useRouter, usePathname } from "next/navigation"
 import { useContext, useEffect } from "react"
 
 export default function CheckAuth() {
-    const { user } = useContext(AuthContext)
- 
+    const { user, setUser } = useContext(AuthContext)
+
     const router = useRouter()
     const pathname = usePathname()
 
     useEffect(() => {
-        if(!user && pathname.startsWith('/dashboard')){
-            router.push('/login')
+        if (!user && pathname.startsWith('/dashboard')) {
+            const _user = sessionStorage.getItem('user')
+            if (_user) {
+                setUser(JSON.parse(_user))
+            } else {
+                router.push('/login')
+            }
         }
-        if(user && pathname.startsWith('/login')){
+        if (user && pathname.startsWith('/login')) {
             router.push('/dashboard')
         }
-    },[user, pathname])//if user changes, we want to kick them off the dashboard
+    }, [user, pathname])//if user changes, we want to kick them off the dashboard
 
     return null
 }
--- context/AuthContext.js
+++ context/AuthContext.js
@@ -1,20 +1,11 @@
 "use client"
-import { createContext, useState, useEffect } from "react";
+import { createContext, useState } from "react";
 
 export const AuthContext = createContext(null);
 
 export default function AuthProvider({ children }) { //children creates a higher order component. Higher order components wrap around other stuff. Not self closing. All wrapped in the higher order component are called children.
     
     const [user, setUser] = useState(null)
-
-    useEffect(()=>{
-        if(!user){
-            const _user = sessionStorage.getItem("user")
-            if(_user){
-                setUser(JSON.parse(_user)) //JSON stringify turns to string, JSON parse turns it into an object
-            }
-        }
-    },[])
     
     const _setUser = (data) => {
         if(data){ //login
